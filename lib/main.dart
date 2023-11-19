8534423b4ae8b1033c1e765ed307dbbdd8789d13
AmirShakouri <forworkandeffamir@gmial.com>
2023-11-20 01:52:11 +0330

    product card (complited)

--- lib/main.dart
+++ lib/main.dart
@@ -26,82 +26,127 @@ class Application extends StatelessWidget {
                 ),
               ),
               child: Column(
+                mainAxisSize: MainAxisSize.max,
+                //crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   const SizedBox(height: 10),
-                  Image.asset('assets/images/iphone.png'),
-                  const Spacer(),
-                  const Text(
-                    'ایفون 13 پرو مکس',
-                    style: TextStyle(
-                      fontFamily: 'SM',
-                      fontSize: 14,
-                    ),
-                  ),
-                  const Spacer(),
-                  Container(
-                    height: 53,
-                    decoration: BoxDecoration(
-                      color: CustomColor.indivatorColor,
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(22),
-                        bottomRight: Radius.circular(22),
+                  Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      Expanded(
+                        child: Container(),
                       ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: CustomColor.indivatorColor,
-                          spreadRadius: -15,
-                          blurRadius: 25,
-                          offset: const Offset(0.0, 15),
+                      Image.asset('assets/images/iphone.png'),
+                      Positioned(
+                        top: 0,
+                        right: 10,
+                        child: Image.asset(
+                          'assets/images/active_fav_product.png',
                         ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          const Text(
-                            'تومان',
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 5,
+                        child: Container(
+                          alignment: AlignmentDirectional.center,
+                          height: 20,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: CustomColor.redColor,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Text(
+                            '%3',
                             style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: "SM",
                               fontSize: 12,
+                              color: Colors.white,
+                              fontFamily: 'SM',
                             ),
                           ),
-                          const SizedBox(width: 9),
-                          const Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                        ),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 10.0, bottom: 5),
+                        child: Text(
+                          'ایفون 13 پرو مکس',
+                          style: TextStyle(
+                            fontFamily: 'SM',
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 53,
+                        decoration: BoxDecoration(
+                          color: CustomColor.indivatorColor,
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(22),
+                            bottomRight: Radius.circular(22),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: CustomColor.indivatorColor,
+                              spreadRadius: -15,
+                              blurRadius: 25,
+                              offset: const Offset(0.0, 15),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
                             children: [
-                              Text(
-                                '20.000.00',
+                              const Text(
+                                'تومان',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontFamily: "SM",
                                   fontSize: 12,
-                                  decoration: TextDecoration.lineThrough,
                                 ),
                               ),
-                              Text(
-                                '20.000.00',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: "SM",
-                                  fontSize: 16,
-                                ),
+                              const SizedBox(width: 9),
+                              const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '20.000.00',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "SM",
+                                      fontSize: 12,
+                                      decoration: TextDecoration.lineThrough,
+                                    ),
+                                  ),
+                                  Text(
+                                    '20.000.00',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "SM",
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
                               ),
+                              const Spacer(),
+                              SizedBox(
+                                width: 24,
+                                child: Image.asset(
+                                  'assets/images/icon_right_arrow_cricle.png',
+                                ),
+                              )
                             ],
                           ),
-                          const Spacer(),
-                          SizedBox(
-                            width: 24,
-                            child: Image.asset(
-                              'assets/images/icon_right_arrow_cricle.png',
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   )
                 ],
               ),
