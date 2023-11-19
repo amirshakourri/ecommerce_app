8a07189827a2812604116a7b5adf7e0af09c13e7
AmirShakouri <forworkandeffamir@gmial.com>
2023-11-19 21:20:55 +0330

    background screen color added to customColor class

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class CustomColor {
   static Color indivatorColor = const Color(0xff3B5EDF);
+  static Color backgroundScreenColor = const Color(0xffEEEEEE);
 }
