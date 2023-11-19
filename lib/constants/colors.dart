b97cdc281541c46ad0616de152683c342f00bf5d
AmirShakouri <forworkandeffamir@gmial.com>
2023-11-19 23:07:41 +0330

    adding colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class CustomColor {
   static Color indivatorColor = const Color(0xff3B5EDF);
   static Color backgroundScreenColor = const Color(0xffEEEEEE);
+  static Color greyColor = const Color(0xff858585);
+  static Color greenColor = const Color(0xff1DB68B);
+  static Color redColor = const Color(0xffD02026);
 }
