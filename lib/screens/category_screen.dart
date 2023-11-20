56b16622b6fef1971a355570e37bf9c069e2cbb3
AmirShakouri <forworkandeffamir@gmial.com>
2023-11-21 01:28:46 +0330

    category screen up

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,76 @@
+import 'package:ecommerce_app/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 46, right: 46, bottom: 32),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Image.asset(
+                        'assets/images/icon_apple_blue.png',
+                      ),
+                      Expanded(
+                        child: Text(
+                          'دسته بندی',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'SB',
+                            color: CustomColor.indivatorColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 44,
+              ),
+              sliver: SliverGrid(
+                delegate: SliverChildBuilderDelegate(
+                  childCount: 10,
+                  (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: CustomColor.indivatorColor,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    );
+                  },
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
