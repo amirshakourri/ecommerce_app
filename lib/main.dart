c0a8eb32039150b0022e4241c6082fec73189b36
AmirShakouri <forworkandeffamir@gmial.com>
2023-11-22 20:54:17 +0330

    bottom navigation bar done

--- lib/main.dart
+++ lib/main.dart
@@ -6,100 +6,137 @@ import 'package:ecommerce_app/screens/category_screen.dart';
 import 'package:ecommerce_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'screens/profile_screen.dart';
+
 void main() {
   runApp(
     const Application(),
   );
 }
 
-class Application extends StatelessWidget {
+class Application extends StatefulWidget {
   const Application({super.key});
 
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  int sellcetedBottomNavigationIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: const ProductListScreen(),
+        //appBar: AppBar(),
+        body: IndexedStack(
+          index: sellcetedBottomNavigationIndex,
+          children: getScreen(),
+        ),
         bottomNavigationBar: ClipRRect(
           child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+            filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
             child: BottomNavigationBar(
+              onTap: (int index) {
+                setState(() {
+                  sellcetedBottomNavigationIndex = index;
+                });
+              },
+              currentIndex: sellcetedBottomNavigationIndex,
               type: BottomNavigationBarType.fixed,
               backgroundColor: Colors.transparent,
               elevation: 0,
-              selectedLabelStyle: const TextStyle(
-                fontFamily: "SM",
-                color: Colors.red,
+              selectedLabelStyle: TextStyle(
+                fontFamily: "SB",
+                color: CustomColor.indivatorColor,
+                fontSize: 10,
               ),
               unselectedLabelStyle: const TextStyle(
-                  //color: Colors.black,
-                  ),
+                fontFamily: "SB",
+                color: Colors.black,
+                fontSize: 10,
+              ),
               items: [
                 BottomNavigationBarItem(
-                  activeIcon:
-                      Image.asset('assets/images/icon_basket_active.png'),
-                  icon: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: CustomColor.indivatorColor,
-                          spreadRadius: -7,
-                          blurRadius: 20,
-                          offset: const Offset(0.0, 10),
-                        )
-                      ],
+                  icon: Image.asset('assets/images/icon_profile.png'),
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child:
+                          Image.asset('assets/images/icon_profile_active.png'),
                     ),
-                    child: Image.asset('assets/images/icon_basket.png'),
                   ),
-                  label: 'خانه',
+                  label: 'حساب کاربری',
                 ),
                 BottomNavigationBarItem(
-                  icon: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: CustomColor.indivatorColor,
-                          spreadRadius: -7,
-                          blurRadius: 20,
-                          offset: const Offset(0.0, 10),
-                        )
-                      ],
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child:
+                          Image.asset('assets/images/icon_basket_active.png'),
                     ),
-                    child: Image.asset('assets/images/icon_basket.png'),
                   ),
+                  icon: Image.asset('assets/images/icon_basket.png'),
                   label: 'سبد خرید',
                 ),
                 BottomNavigationBarItem(
-                  icon: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: CustomColor.indivatorColor,
-                          spreadRadius: -7,
-                          blurRadius: 20,
-                          offset: const Offset(0.0, 10),
-                        )
-                      ],
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child:
+                          Image.asset('assets/images/icon_category_active.png'),
                     ),
-                    child: Image.asset('assets/images/icon_basket.png'),
                   ),
+                  icon: Image.asset('assets/images/icon_category.png'),
                   label: 'پروفایل',
                 ),
                 BottomNavigationBarItem(
-                  icon: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: CustomColor.indivatorColor,
-                          spreadRadius: -7,
-                          blurRadius: 20,
-                          offset: const Offset(0.0, 10),
-                        )
-                      ],
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child: Image.asset('assets/images/icon_home_active.png'),
                     ),
-                    child: Image.asset('assets/images/icon_basket.png'),
                   ),
+                  icon: Image.asset('assets/images/icon_home.png'),
                   label: 'تنظیمات',
                 ),
               ],
@@ -109,4 +146,13 @@ class Application extends StatelessWidget {
       ),
     );
   }
+}
+
+List<Widget> getScreen() {
+  return <Widget>[
+    const ProductListScreen(),
+    const CategoryScreen(),
+    const HomeScreen(),
+    const ProfileScreen(),
+  ];
 }
