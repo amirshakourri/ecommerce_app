b4fc9117466333ce11bfd0c25c04de8c46b6a9b2
x1mir <amirshakouridesign@gmail.com>
2023-11-26 22:39:07 +0330

    untaged codes & CardScreen add to getScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:ecommerce_app/screens/card_screen.dart';
 import 'package:ecommerce_app/screens/category_list_screen.dart';
 import 'package:ecommerce_app/screens/category_screen.dart';
 import 'package:ecommerce_app/screens/home_screen.dart';
@@ -6,6 +7,7 @@ import 'package:ecommerce_app/screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 //import 'constants/colors.dart';
+import 'constants/colors.dart';
 import 'screens/profile_screen.dart';
 
 void main() {
@@ -28,122 +30,122 @@ class _ApplicationState extends State<Application> {
     return MaterialApp(
       home: Scaffold(
         //appBar: AppBar(),
-        body: const ProductDetailScreen(),
-        // body: IndexedStack(
-        //   index: sellcetedBottomNavigationIndex,
-        //   children: getScreen(),
-        // ),
-        // bottomNavigationBar: ClipRRect(
-        //   child: BackdropFilter(
-        //     filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-        //     child: BottomNavigationBar(
-        //       onTap: (int index) {
-        //         setState(() {
-        //           sellcetedBottomNavigationIndex = index;
-        //         });
-        //       },
-        //       currentIndex: sellcetedBottomNavigationIndex,
-        //       type: BottomNavigationBarType.fixed,
-        //       backgroundColor: Colors.transparent,
-        //       elevation: 0,
-        //       selectedLabelStyle: TextStyle(
-        //         fontFamily: "SB",
-        //         color: CustomColor.indivatorColor,
-        //         fontSize: 10,
-        //       ),
-        //       unselectedLabelStyle: const TextStyle(
-        //         fontFamily: "SB",
-        //         color: Colors.black,
-        //         fontSize: 10,
-        //       ),
-        //       items: [
-        //         BottomNavigationBarItem(
-        //           icon: Image.asset('assets/images/icon_profile.png'),
-        //           activeIcon: Padding(
-        //             padding: const EdgeInsets.only(bottom: 4.0),
-        //             child: Container(
-        //               decoration: BoxDecoration(
-        //                 boxShadow: [
-        //                   BoxShadow(
-        //                     color: CustomColor.indivatorColor,
-        //                     spreadRadius: -7,
-        //                     blurRadius: 20,
-        //                     offset: const Offset(0.0, 10),
-        //                   )
-        //                 ],
-        //               ),
-        //               child:
-        //                   Image.asset('assets/images/icon_profile_active.png'),
-        //             ),
-        //           ),
-        //           label: 'حساب کاربری',
-        //         ),
-        //         BottomNavigationBarItem(
-        //           activeIcon: Padding(
-        //             padding: const EdgeInsets.only(bottom: 4.0),
-        //             child: Container(
-        //               decoration: BoxDecoration(
-        //                 boxShadow: [
-        //                   BoxShadow(
-        //                     color: CustomColor.indivatorColor,
-        //                     spreadRadius: -7,
-        //                     blurRadius: 20,
-        //                     offset: const Offset(0.0, 10),
-        //                   )
-        //                 ],
-        //               ),
-        //               child:
-        //                   Image.asset('assets/images/icon_basket_active.png'),
-        //             ),
-        //           ),
-        //           icon: Image.asset('assets/images/icon_basket.png'),
-        //           label: 'سبد خرید',
-        //         ),
-        //         BottomNavigationBarItem(
-        //           activeIcon: Padding(
-        //             padding: const EdgeInsets.only(bottom: 4.0),
-        //             child: Container(
-        //               decoration: BoxDecoration(
-        //                 boxShadow: [
-        //                   BoxShadow(
-        //                     color: CustomColor.indivatorColor,
-        //                     spreadRadius: -7,
-        //                     blurRadius: 20,
-        //                     offset: const Offset(0.0, 10),
-        //                   )
-        //                 ],
-        //               ),
-        //               child:
-        //                   Image.asset('assets/images/icon_category_active.png'),
-        //             ),
-        //           ),
-        //           icon: Image.asset('assets/images/icon_category.png'),
-        //           label: 'پروفایل',
-        //         ),
-        //         BottomNavigationBarItem(
-        //           activeIcon: Padding(
-        //             padding: const EdgeInsets.only(bottom: 4.0),
-        //             child: Container(
-        //               decoration: BoxDecoration(
-        //                 boxShadow: [
-        //                   BoxShadow(
-        //                     color: CustomColor.indivatorColor,
-        //                     spreadRadius: -7,
-        //                     blurRadius: 20,
-        //                     offset: const Offset(0.0, 10),
-        //                   )
-        //                 ],
-        //               ),
-        //               child: Image.asset('assets/images/icon_home_active.png'),
-        //             ),
-        //           ),
-        //           icon: Image.asset('assets/images/icon_home.png'),
-        //           label: 'تنظیمات',
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
+        //body: const CardScreen(),
+        body: IndexedStack(
+          index: sellcetedBottomNavigationIndex,
+          children: getScreen(),
+        ),
+        bottomNavigationBar: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+            child: BottomNavigationBar(
+              onTap: (int index) {
+                setState(() {
+                  sellcetedBottomNavigationIndex = index;
+                });
+              },
+              currentIndex: sellcetedBottomNavigationIndex,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              selectedLabelStyle: TextStyle(
+                fontFamily: "SB",
+                color: CustomColor.indivatorColor,
+                fontSize: 10,
+              ),
+              unselectedLabelStyle: const TextStyle(
+                fontFamily: "SB",
+                color: Colors.black,
+                fontSize: 10,
+              ),
+              items: [
+                BottomNavigationBarItem(
+                  icon: Image.asset('assets/images/icon_profile.png'),
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child:
+                          Image.asset('assets/images/icon_profile_active.png'),
+                    ),
+                  ),
+                  label: 'حساب کاربری',
+                ),
+                BottomNavigationBarItem(
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child:
+                          Image.asset('assets/images/icon_basket_active.png'),
+                    ),
+                  ),
+                  icon: Image.asset('assets/images/icon_basket.png'),
+                  label: 'سبد خرید',
+                ),
+                BottomNavigationBarItem(
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child:
+                          Image.asset('assets/images/icon_category_active.png'),
+                    ),
+                  ),
+                  icon: Image.asset('assets/images/icon_category.png'),
+                  label: 'پروفایل',
+                ),
+                BottomNavigationBarItem(
+                  activeIcon: Padding(
+                    padding: const EdgeInsets.only(bottom: 4.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: CustomColor.indivatorColor,
+                            spreadRadius: -7,
+                            blurRadius: 20,
+                            offset: const Offset(0.0, 10),
+                          )
+                        ],
+                      ),
+                      child: Image.asset('assets/images/icon_home_active.png'),
+                    ),
+                  ),
+                  icon: Image.asset('assets/images/icon_home.png'),
+                  label: 'تنظیمات',
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -152,7 +154,7 @@ class _ApplicationState extends State<Application> {
 List<Widget> getScreen() {
   return <Widget>[
     const ProductListScreen(),
-    const CategoryScreen(),
+    const CardScreen(),
     const HomeScreen(),
     const ProfileScreen(),
   ];
