401e8ea2c3448f45992133b8cc08b69734425485
x1mir <amirshakouridesign@gmail.com>
2023-12-10 02:23:55 +0330

    dependency injection file

---
+++ lib/di/di.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/data/datasource/authentication_datasource.dart';
+import 'package:ecommerce_app/data/repository/authentication_repository.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+var locator = GetIt.instance;
+
+Future<void> getItInit() async {
+  //
+  locator.registerSingleton<Dio>(
+    Dio(BaseOptions(baseUrl: "http://startflutter.ir/api/")),
+  );
+
+  locator.registerSingleton<SharedPreferences>(
+      await SharedPreferences.getInstance());
+      
+  // datesources
+  locator
+      .registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
+
+  // repositories
+  locator.registerFactory<IAuthRepository>(() => AuthenticationRepository());
+}
