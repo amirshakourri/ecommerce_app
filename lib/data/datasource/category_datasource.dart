e38044a6fcf345943b02bf1f848e20070d46adb7
x1mir <amirshakouridesign@gmail.com>
2023-12-28 13:02:11 +0330

    "datesource category appended to datasource file"

---
+++ lib/data/datasource/category_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/data/model/category.dart';
+import 'package:ecommerce_app/di/di.dart';
+import 'package:ecommerce_app/util/api_exception.dart';
+
+abstract class ICategoryDatasource {
+  Future<List<Category>> getCategories();
+}
+
+class CategoryRemoteDatasourse extends ICategoryDatasource {
+  final Dio _dio = locator.get();
+
+  @override
+  Future<List<Category>> getCategories() async {
+    try {
+      var response = await _dio.get('collections/category/records');
+      return response.data['items']
+          .map<Category>(
+            (jsonObject) => Category.fromMapJson(jsonObject),
+          )
+          .toList();
+    } on DioException catch (ex) {
+      throw ApiException(ex.response?.statusCode, ex.response?.data['message']);
+    } catch (ex) {
+      throw ApiException(0, 'unknown error');
+    }
+  }
+}
