46d629569f8048f2d87730a4b265834c8909184d
x1mir <amirshakouridesign@gmail.com>
2024-01-09 15:53:47 +0330

    "bannerList become final and BannerSlider constructor become const"

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class BannerSlider extends StatelessWidget {
-  List<Banners> bannerList;
-  BannerSlider(this.bannerList, {super.key});
+  final List<Banners> bannerList;
+  const BannerSlider(this.bannerList, {super.key});
 
   @override
   Widget build(BuildContext context) {
