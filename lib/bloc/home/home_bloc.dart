773c4b54d58522de6d0a1fd3b9f24951399475e7
x1mir <amirshakouridesign@gmail.com>
2024-01-11 21:12:12 +0330

    some update

--- lib/bloc/home/home_bloc.dart
+++ lib/bloc/home/home_bloc.dart
@@ -1,17 +1,23 @@
 import 'package:ecommerce_app/bloc/home/home_event.dart';
 import 'package:ecommerce_app/bloc/home/home_state.dart';
 import 'package:ecommerce_app/data/repository/banner_repository.dart';
+import 'package:ecommerce_app/data/repository/category_repository.dart';
+import 'package:ecommerce_app/data/repository/product_repository.dart';
 import 'package:ecommerce_app/di/di.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final IBannerRepository _bannerRepository = locator.get();
-  
+  final ICategoryRepository _categoryRepository = locator.get();
+  final IProductRepository _iProductRepository = locator.get();
+
   HomeBloc() : super(HomeInitState()) {
     on<HomeGetInitilzeData>((event, emit) async {
-      emit(HomeLodingState());
       var bannerList = await _bannerRepository.getBanners();
-      emit(HomeRequsetSuccessState(bannerList));
+      var categoryList = await _categoryRepository.getCategories();
+      var productList = await _iProductRepository.getProducts();
+      emit(HomeLodingState());
+      emit(HomeRequsetSuccessState(bannerList, categoryList, productList));
     });
   }
 }
--- lib/bloc/home/home_state.dart
+++ lib/bloc/home/home_state.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecommerce_app/data/model/banner.dart';
-
+import 'package:ecommerce_app/data/model/category.dart';
+import 'package:ecommerce_app/data/model/product.dart';
 
 abstract class HomeState {}
 
@@ -10,5 +11,7 @@ class HomeInitState extends HomeState {}
 
 class HomeRequsetSuccessState extends HomeState {
   Either<String, List<Banners>> bannerList;
-  HomeRequsetSuccessState(this.bannerList);
+  Either<String, List<Category>> categoryList;
+  Either<String, List<Product>> productList;
+  HomeRequsetSuccessState(this.bannerList, this.categoryList, this.productList);
 }
