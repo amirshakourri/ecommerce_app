681c9bbb7956daa983a2a8db675f7b4bc31439fa
x1mir <amirshakouridesign@gmail.com>
2024-01-11 21:00:45 +0330

    adding product di

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -2,9 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:ecommerce_app/data/datasource/authentication_datasource.dart';
 import 'package:ecommerce_app/data/datasource/banner_datasource.dart';
 import 'package:ecommerce_app/data/datasource/category_datasource.dart';
+import 'package:ecommerce_app/data/datasource/product_datasource.dart';
 import 'package:ecommerce_app/data/repository/authentication_repository.dart';
 import 'package:ecommerce_app/data/repository/banner_repository.dart';
 import 'package:ecommerce_app/data/repository/category_repository.dart';
+import 'package:ecommerce_app/data/repository/product_repository.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -27,8 +29,10 @@ Future<void> getItInit() async {
   locator
       .registerFactory<ICategoryDatasource>(() => CategoryRemoteDatasourse());
   locator.registerFactory<IBannerDatasource>(() => BannerRemoteDatasource());
+  locator.registerFactory<IProductDatasource>(() => ProductRemoteDatasource());
   // repositories
   locator.registerFactory<IAuthRepository>(() => AuthenticationRepository());
   locator.registerFactory<ICategoryRepository>(() => CategoryRepository());
   locator.registerFactory<IBannerRepository>(() => BannerRepository());
+  locator.registerFactory<IProductRepository>(() => ProductRepository());
 }
